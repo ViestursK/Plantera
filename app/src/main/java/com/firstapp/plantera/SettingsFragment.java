cc298f989e1135425bdd0b90938b4273c4457d1d
Valters-Kalcenaus <valters2002@gmail.com>
2023-10-21 20:49:39 +0300

    Sign-up with Google
    created google sign-up, and finished up the settings fragment.

--- app/src/main/java/com/firstapp/plantera/SettingsFragment.java
+++ app/src/main/java/com/firstapp/plantera/SettingsFragment.java
@@ -1,64 +1,114 @@
 package com.firstapp.plantera;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.squareup.picasso.Picasso;
+
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        Button signOutButton = view.findViewById(R.id.sign_out_button);
+        CardView signOutCard = view.findViewById(R.id.sign_out_card);
+
+        // Retrieve the signed-in Google account
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(requireContext());
+
+        if (account != null) {
+            // Display user information in the UI
+            TextView userNameTextView = view.findViewById(R.id.user_name_text_view);
+            TextView userEmailTextView = view.findViewById(R.id.user_email_text_view);
+            ImageView userImageView = view.findViewById(R.id.user_image_view);
+
+            String userName = account.getDisplayName();
+            String userEmail = account.getEmail();
+            String userPhotoUrl = String.valueOf(account.getPhotoUrl());
+
+            userNameTextView.setText(userName);
+            userEmailTextView.setText(userEmail);
+
+            // Load and display the user's profile picture using Picasso
+            if (userPhotoUrl != null) {
+                Picasso.get().load(userPhotoUrl).into(userImageView);
+            }
         }
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(requireContext(), gso); // Initialize GoogleSignInClient
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform sign-out logic here
+                signOut();
+            }
+        });
+
+        signOutCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform sign-out logic here when the CardView is clicked
+                signOut();
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    private void signOut() {
+        // Sign out the user
+        gsc.signOut().addOnCompleteListener(requireActivity(), task -> {
+            // Clear the "signed_in" flag
+            clearSignedInFlag();
+            // Start SignInActivity
+            startSignInActivity();
+
+            // Show a "Signed out" toast message
+            showSignedOutToast();
+        });
+    }
+
+    private void showSignedOutToast() {
+        Toast.makeText(requireContext(), "Signed out", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+    private void clearSignedInFlag() {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("user_pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("signed_in");
+        editor.apply();
+    }
+
+    private void startSignInActivity() {
+        Intent intent = new Intent(requireContext(), SignInActivity.class);
+        startActivity(intent);
+        requireActivity().finish();
     }
 }
